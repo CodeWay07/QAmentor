55e5682d9df4a00505e3d78b8c963dfc51774084
Maxim Bugăescu <maxim.bugaescu@proton.me>
2025-07-03 02:44:51 +0300

    Arrays and Strings classes add

--- qa/src/Main.java
+++ qa/src/Main.java
@@ -3,28 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-//       Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter your numbers: ");
-//        int input = scanner.nextInt();
-//
-//        Numbers.oddEvenChecker(input);
-//        Numbers.sum(input);
+/*
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("Enter your numbers: ");
+       int input = scanner.nextInt();
 
-//        int[] arr1 = {10, 5, 20, 15, 2};
-//        int[] arr2 = {1, 2, 3, 4, 5};
-//        int[] arr3 = {7, 7, 3, 1, 9};
-//        int[] arr4 = {5, 5, 5, 5};
-//        int[] arr5 = {10, 30, 20, 30, 5};
-//        int[] arr6 = {42};
-//        int[] arr7 = {};
-//
-//        Arrays.greaterNumber(arr1);
-//        Arrays.greaterNumber(arr2);
-//        Arrays.greaterNumber(arr3);
-//        Arrays.greaterNumber(arr4);
-//        Arrays.greaterNumber(arr5);
-//        Arrays.greaterNumber(arr6);
-//        Arrays.greaterNumber(arr7);
+        Numbers.oddEvenChecker(input);
+        Numbers.sum(input);
+
+        int[] arr1 = {10, 5, 20, 15, 2};
+        int[] arr2 = {1, 2, 3, 4, 5};
+        int[] arr3 = {7, 7, 3, 1, 9};
+        int[] arr4 = {5, 5, 5, 5};
+        int[] arr5 = {10, 30, 20, 30, 5};
+        int[] arr6 = {42};
+        int[] arr7 = {};
+
+        Arrays.greaterNumber(arr1);
+        Arrays.greaterNumber(arr2);
+        Arrays.greaterNumber(arr3);
+        Arrays.greaterNumber(arr4);
+        Arrays.greaterNumber(arr5);
+        Arrays.greaterNumber(arr6);
+        Arrays.greaterNumber(arr7);
+ */
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your String: ");
